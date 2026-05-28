551e3c66bff3567dc0a1e195e29464f231f3192a
re7490 <ralonsopadilla995@gmail.com>
2026-05-28 12:47:28 -0400

    Funcion 2 OK?

--- P2.rkt
+++ P2.rkt
@@ -1,1 +1,28 @@
 #lang scheme
+
+(define (crear-gusano vulnerabilidad)
+    (lambda (memoria)
+        (letrec 
+            ((loop (lambda (m)
+                (cond
+                    ((null? m) '())
+                    ((list? (car m))
+                        (cons (loop (car m)) (loop (cdr m)))
+                    )
+                    ((vulnerabilidad (car m))
+                        (cons (car m) (loop (cdr m)))
+                    )
+                    (else
+                        (cons 'X (loop (cdr m)))
+                    )
+                )
+            )))
+            (loop memoria)
+        )   
+    )
+)
+
+(define (vulnerable-a-viejos? x) (<= x 15))
+(define gusano-windowsXP (crear-gusano vulnerable-a-viejos?))
+
+(gusano-windowsXP '(1 2 3 (1 3 8) 15 (20 6 76 09 90 21 3) 4 5 9 8 0 20 36 7 76 7 8 67))
